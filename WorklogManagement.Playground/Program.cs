282cec5fd92444d09d4ece3fed49ed2ba1712f8d
Zinoberous <alexanderkarge@outlook.de>
2023-12-02 23:48:22 +0100

    ...

--- WorklogManagement.Playground/Program.cs
+++ WorklogManagement.Playground/Program.cs
@@ -1,5 +1,5 @@
 ﻿//var options = new DbContextOptionsBuilder<WorklogManagementContext>()
-//    .UseSqlServer("Server=lexnarf.dns.navy,52341;Database=DevWorklogManagement;User Id=Lex;Password=ddQ_H9wKM@1D#JoGFFt6;TrustServerCertificate=true;")
+//    .UseSqlServer("Server=lexnarf.dns.navy,52341;Database=DevWorklogManagement;User Id=Lex;Password=<password>;TrustServerCertificate=true;")
 //    .Options;
 
 //using WorklogManagementContext context = new(options);
