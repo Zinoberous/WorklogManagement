06a0076c131b0e4bc326b0466fbbf6472a5a6817
Zinoberous <alexanderkarge@outlook.de>
2024-04-15 18:46:19 +0200

    Überstunden jetzt nach Büro und Mobil gesplitet

--- WorklogManagement.API/Controllers/StatisticsController.cs
+++ WorklogManagement.API/Controllers/StatisticsController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Concurrent;
+using System.Globalization;
 using WorklogManagement.API.Enums;
 using WorklogManagement.DataAccess.Context;
 
@@ -16,15 +18,152 @@ namespace WorklogManagement.API.Controllers
         [HttpGet("overtime")]
         public async Task<IActionResult> GetOvertimeSeconds()
         {
-            var requiredWorktime = await _context.CalendarEntries
+            var totalOvertimeSeconds = 0;
+            var officeOvertimeSeconds = 0;
+            var mobileOvertimeSeconds = 0;
+
+            ConcurrentDictionary<DateOnly, int> unknownOvertimes = new();
+
+            var relevantTypes = new[] { CalendarEntryType.WorkTime, CalendarEntryType.Office, CalendarEntryType.Mobile };
+
+            var days =  await _context.CalendarEntries
+                .Where(x => relevantTypes.Contains((CalendarEntryType)x.CalendarEntryTypeId))
+                .GroupBy(x => x.Date)
+                .ToDictionaryAsync(x => x.Key, x => x.ToList());
+
+            var officeDays = days
+                .Select(x => x.Value)
+                .Where(x =>
+                    x.Select(x => (CalendarEntryType)x.CalendarEntryTypeId).Order().SequenceEqual([CalendarEntryType.Office]) ||
+                    x.Select(x => (CalendarEntryType)x.CalendarEntryTypeId).Order().SequenceEqual([CalendarEntryType.WorkTime, CalendarEntryType.Office]) ||
+                    x.Select(x => (CalendarEntryType)x.CalendarEntryTypeId).Order().SequenceEqual([CalendarEntryType.Office, CalendarEntryType.Mobile]))
+                .SelectMany(x => x);
+
+            var totalOfficeWorktime = officeDays
+                .Where(x => x.CalendarEntryTypeId == (int)CalendarEntryType.Office)
+                .Sum(x => x.DurationSeconds);
+
+            var requiredOfficeWorktime = officeDays
                 .Where(x => x.CalendarEntryTypeId == (int)CalendarEntryType.WorkTime)
-                .SumAsync(x => x.DurationSeconds);
+                .Sum(x => x.DurationSeconds);
 
-            var totalWorktime = await _context.CalendarEntries
-                .Where(x => x.CalendarEntryTypeId == (int)CalendarEntryType.Office || x.CalendarEntryTypeId == (int)CalendarEntryType.Mobile)
-                .SumAsync(x => x.DurationSeconds);
+            officeOvertimeSeconds = totalOfficeWorktime - requiredOfficeWorktime;
 
-            var overtime = totalWorktime - requiredWorktime;
+            totalOvertimeSeconds = officeOvertimeSeconds;
+
+            var mobileDays = days
+                .Select(x => x.Value)
+                .Where(x =>
+                    x.Select(x => (CalendarEntryType)x.CalendarEntryTypeId).Order().SequenceEqual([CalendarEntryType.Mobile]) ||
+                    x.Select(x => (CalendarEntryType)x.CalendarEntryTypeId).Order().SequenceEqual([CalendarEntryType.WorkTime, CalendarEntryType.Mobile]) ||
+                    x.Select(x => (CalendarEntryType)x.CalendarEntryTypeId).Order().SequenceEqual([CalendarEntryType.Office, CalendarEntryType.Mobile]))
+                .SelectMany(x => x);
+
+            var totalMobileWorktime = mobileDays
+                .Where(x => x.CalendarEntryTypeId == (int)CalendarEntryType.Mobile)
+                .Sum(x => x.DurationSeconds);
+
+            var requiredMobileWorktime = mobileDays
+                .Where(x => x.CalendarEntryTypeId == (int)CalendarEntryType.WorkTime)
+                .Sum(x => x.DurationSeconds);
+
+            mobileOvertimeSeconds = totalMobileWorktime - requiredMobileWorktime;
+
+            totalOvertimeSeconds += mobileOvertimeSeconds;
+
+            var unsureDays = days
+                .Where(x =>
+                    x.Value.Select(x => (CalendarEntryType)x.CalendarEntryTypeId).Order().SequenceEqual([CalendarEntryType.WorkTime]) ||
+                    x.Value.Select(x => (CalendarEntryType)x.CalendarEntryTypeId).Order().SequenceEqual([CalendarEntryType.WorkTime, CalendarEntryType.Office, CalendarEntryType.Mobile]));
+
+            static int SecondsFromNote(string note)
+            {
+                // Format: <Titel>: HH:mm | ... => Titel = Büro, Mobil oder Über / HH:mm = timeStr / | ... = optionale Zusatznotiz
+                var timeStr = string.Join(':', note.Split('|')[0].Split(':').Skip(1)).Trim();
+                return (int)TimeSpan.Parse(timeStr, CultureInfo.InvariantCulture).TotalSeconds;
+            }
+
+            Parallel.ForEach(unsureDays, day =>
+            {
+                var date = day.Key;
+                var entries = day.Value;
+
+                var worktimeSeconds = entries.SingleOrDefault(x => (CalendarEntryType)x.CalendarEntryTypeId == CalendarEntryType.WorkTime)?.DurationSeconds ?? 0;
+                var officeSeconds = entries.SingleOrDefault(x => (CalendarEntryType)x.CalendarEntryTypeId == CalendarEntryType.Office)?.DurationSeconds ?? 0;
+                var mobileSeconds = entries.SingleOrDefault(x => (CalendarEntryType)x.CalendarEntryTypeId == CalendarEntryType.Mobile)?.DurationSeconds ?? 0;
+
+                var overtimeSeconds = (officeSeconds + mobileSeconds) - worktimeSeconds;
+
+                if (overtimeSeconds == 0)
+                {
+                    return;
+                }
+
+                Interlocked.Add(ref totalOvertimeSeconds, overtimeSeconds);
+
+                // Minusstunden
+                if (overtimeSeconds < 0)
+                {
+                    var worktimeNote = entries.Single(x => (CalendarEntryType)x.CalendarEntryTypeId == CalendarEntryType.WorkTime).Note;
+
+                    if (!string.IsNullOrWhiteSpace(worktimeNote) && worktimeNote.StartsWith("Büro:"))
+                    {
+                        var specifiedSeconds = SecondsFromNote(worktimeNote);
+
+                        Interlocked.Add(ref officeOvertimeSeconds, -specifiedSeconds);
+                        Interlocked.Add(ref mobileOvertimeSeconds, -(Math.Abs(overtimeSeconds) - specifiedSeconds));
+                    }
+                    else if (!string.IsNullOrWhiteSpace(worktimeNote) && worktimeNote.StartsWith("Mobil:"))
+                    {
+                        var specifiedSeconds = SecondsFromNote(worktimeNote);
+
+                        Interlocked.Add(ref mobileOvertimeSeconds, -specifiedSeconds);
+                        Interlocked.Add(ref officeOvertimeSeconds, -(Math.Abs(overtimeSeconds) - specifiedSeconds));
+                    }
+                    else
+                    {
+                        unknownOvertimes.TryAdd(date, overtimeSeconds);
+                    }
+                }
+                // Überstunden
+                else if (overtimeSeconds > 0)
+                {
+                    var officeNote = entries.Single(x => (CalendarEntryType)x.CalendarEntryTypeId == CalendarEntryType.Office).Note;
+
+                    if (!string.IsNullOrWhiteSpace(officeNote) && officeNote.StartsWith("Über:"))
+                    {
+                        var specifiedSeconds = SecondsFromNote(officeNote);
+
+                        Interlocked.Add(ref officeOvertimeSeconds, specifiedSeconds);
+
+                        overtimeSeconds -= specifiedSeconds;
+                    }
+
+                    var mobileNote = entries.Single(x => (CalendarEntryType)x.CalendarEntryTypeId == CalendarEntryType.Mobile).Note;
+
+                    if (!string.IsNullOrWhiteSpace(mobileNote) && mobileNote.StartsWith("Über:"))
+                    {
+                        var specifiedSeconds = SecondsFromNote(mobileNote);
+
+                        Interlocked.Add(ref mobileOvertimeSeconds, specifiedSeconds);
+
+                        overtimeSeconds -= specifiedSeconds;
+                    }
+
+                    if (overtimeSeconds != 0)
+                    {
+                        unknownOvertimes.TryAdd(date, overtimeSeconds);
+                    }
+                }
+            });
+
+            var overtime = new
+            {
+                total = totalOvertimeSeconds,
+                office = officeOvertimeSeconds,
+                mobile = mobileOvertimeSeconds,
+                unknown = unknownOvertimes
+            };
 
             return Ok(overtime);
         }
